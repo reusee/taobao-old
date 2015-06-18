733b1028e1e821a920a9bb1c17900f90eb8a006f
reus <reusee@gmail.com>
2015-06-18 23:28:35 +0800

    do not verify proxy

--- free_proxy.go
+++ free_proxy.go
@@ -29,19 +29,65 @@ func provideFreeProxyClients(clients chan<- *http.Client) {
 				},
 				Timeout: time.Second * 8,
 			}
-			done := make(chan struct{})
-			go func() {
-				_, err = hcutil.GetBytes(client, "http://www.taobao.com")
-				if err == nil {
-					close(done)
+			/*
+				if testClient(client, addr) {
+					clients <- client
 				}
-			}()
-			select {
-			case <-time.After(time.Second * 4):
-			case <-done:
-				pt("client %s ok\n", addr)
-				clients <- client
-			}
+			*/
+			clients <- client
 		}
 	}
+
+	/*
+		for page := 1; page <= 300; page++ {
+			pageUrl := sp("http://www.kuaidaili.com/proxylist/%d/", page)
+			bs, err := hcutil.GetBytes(http.DefaultClient, pageUrl)
+			if err != nil {
+				pt("error getting %s: %v\n", pageUrl, err)
+				continue
+			}
+			doc, err := goquery.NewDocumentFromReader(bytes.NewReader(bs))
+			if err != nil {
+				pt("error parsing %s: %v\n", pageUrl, err)
+				continue
+			}
+			doc.Find("div#list table tbody tr").Each(func(i int, se *goquery.Selection) {
+				tds := se.Find("td")
+				addr := sp("%s:%s", tds.Get(0).FirstChild.Data, tds.Get(1).FirstChild.Data)
+				proxyUrl, err := url.Parse(addr)
+				ce(err, sp("parse http proxy url %s", addr))
+				client := &http.Client{
+					Transport: &http.Transport{
+						Proxy: http.ProxyURL(proxyUrl),
+					},
+					Timeout: time.Second * 8,
+				}
+				if testClient(client, addr) {
+					clients <- client
+				}
+			})
+		}
+	*/
+}
+
+func testClient(client *http.Client, addr string) bool {
+	pt("testing proxy %s\n", addr)
+	done := make(chan struct{})
+	go func() {
+		_, err := hcutil.GetBytes(client, "http://www.taobao.com")
+		if err == nil {
+			close(done)
+		} else {
+			pt("client get error %s: %v\n", addr, err)
+		}
+	}()
+	select {
+	case <-time.After(time.Second * 8):
+		pt("client test timeout %s\n", addr)
+	case <-done:
+		pt("client %s ok\n", addr)
+		return true
+	}
+	pt("client %s not ok\n", addr)
+	return false
 }
