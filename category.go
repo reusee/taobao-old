f56235a6c4d81137b3fd923e21c8c7a7fe1229ed
reus <reusee@gmail.com>
2015-06-19 15:34:10 +0800

    do not collect categories cocurrently

--- category.go
+++ category.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"net/http"
 	"strconv"
 
 	"gopkg.in/mgo.v2"
@@ -12,22 +13,25 @@ import (
 
 type Cat struct {
 	Cat  int
+	Name string
 	Subs []Cat
 }
 
 func collectCategories(db *mgo.Database) {
-	clientsIn, _, clientsOut, killClientsChan := ClientsProvider()
-	defer close(killClientsChan)
-
-	var collectCategory func(cat string) Cat
-	collectCategory = func(cat string) (ret Cat) {
+	collected := make(map[string]bool)
+	var collectCategory func(cat, name string) Cat
+	collectCategory = func(cat, name string) (ret Cat) {
+		if collected[cat] {
+			panic(sp("loop %s %s", cat, name))
+		}
+		client := http.DefaultClient
 		if cat != "" {
 			id, err := strconv.Atoi(cat)
 			ce(err, sp("parse cat id %s", cat))
 			ret.Cat = id
+			ret.Name = name
 		}
 		for {
-			client := <-clientsOut
 			bs, err := hcutil.GetBytes(client, sp("http://s.taobao.com/list?cat=%s", cat))
 			if err != nil {
 				continue
@@ -55,32 +59,21 @@ func collectCategories(db *mgo.Database) {
 			if err != nil {
 				continue
 			}
-			clientsIn <- client
-			subs := []string{}
 			for _, e := range nav.Common {
 				if e.Text == "相关分类" {
 					for _, sub := range e.Sub {
 						pt("%s %s\n", sub.Text, sub.Value)
-						subs = append(subs, sub.Value)
+						ret.Subs = append(ret.Subs, collectCategory(sub.Value, sub.Text))
 					}
 				}
 			}
-			retsChan := make(chan Cat)
-			for _, sub := range subs {
-				sub := sub
-				go func() {
-					retsChan <- collectCategory(sub)
-				}()
-			}
-			for i := 0; i < len(subs); i++ {
-				ret.Subs = append(ret.Subs, <-retsChan)
-			}
 			break
 		}
+		collected[cat] = true
 		return
 	}
 
-	root := collectCategory("")
+	root := collectCategory("", "")
 
 	catsColle := db.C("cats")
 	err := catsColle.EnsureIndex(mgo.Index{
