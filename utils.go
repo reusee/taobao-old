10d33a761ac375766b12a3e83d214f74f82f4c5f
reus <reusee@gmail.com>
2015-06-20 01:27:07 +0800

    add missing files

---
+++ utils.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"net/url"
+	"sort"
+
+	"gopkg.in/mgo.v2"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func dumpUrl(rawUrl string) {
+	u, err := url.Parse(rawUrl)
+	ce(err, "parse url")
+	query := u.Query()
+	var keys []string
+	for k := range query {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	for _, k := range keys {
+		pt("%s -> %v\n", k, query[k])
+	}
+}
+
+func dumpDoc(doc *goquery.Document) {
+	html, _ := doc.Html()
+	pt("%s\n", html)
+}
+
+func ignoreExistsColle(err error) error {
+	if err, ok := err.(*mgo.QueryError); ok {
+		if err.Message == "collection already exists" {
+			return nil
+		}
+	}
+	return err
+}
+
+func allowDup(err error) error {
+	if err, ok := err.(*mgo.LastError); ok {
+		if err.Code == 11000 {
+			return nil
+		}
+	}
+	return err
+}
