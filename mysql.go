0f2433f276b2e4cc9753e53b58beccfcf5f3ecff
reus <reusee@gmail.com>
2015-06-23 23:23:37 +0800

    use category id in Item struct instead of job.Cat

--- mysql.go
+++ mysql.go
@@ -114,13 +114,9 @@ func (m *Mysql) AddItems(items []Item, job Job) (err error) {
 			date, nid, price, sales, comments) VALUES (?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE price = ?, sales = ?, comments = ?`,
 			m.date4mysql, nid, price, sales, comments, price, sales, comments)
 		ce(err, "insert item stats")
-		//item sources
-		//_, err = m.db.Exec(`INSERT INTO item_sources (date, nid, cat, page)
-		//	VALUES (?, ?, ?, ?) ON DUPLICATE KEY UPDATE date=date`, m.date4mysql, nid, job.Cat, job.Page)
-		//ce(err, "insert item sources")
 		//item cats
 		_, err = m.db.Exec(`INSERT INTO item_cats (nid, cat)
-			VALUES (?, ?) ON DUPLICATE KEY UPDATE nid=nid`, nid, job.Cat)
+			VALUES (?, ?) ON DUPLICATE KEY UPDATE nid=nid`, nid, item.Category)
 		ce(err, "insert item cats")
 	}
 	return
