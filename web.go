0bcfe397a57c5be47c151702f270be9b30f79814
reus <reusee@gmail.com>
2015-06-27 12:36:56 +0800

    jobs tracing interface

--- web.go
+++ web.go
@@ -1,52 +1,46 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
-	"time"
-
-	"github.com/gorilla/websocket"
 )
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  4096,
-	WriteBufferSize: 1024,
+type TraceInfo struct {
+	What    string
+	Entries []*Entry
 }
 
 func web() {
 	http.Handle("/", http.FileServer(http.Dir("./web")))
 
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		defer lp()
-		conn, err := upgrader.Upgrade(w, r, nil)
-		ce(err, "upgrade")
-		/*
-			for {
-				what, msg, err := conn.ReadMessage()
-				if err == io.EOF {
-					return
-				}
-				ce(err, "read")
-				if what == websocket.CloseMessage {
-					return
-				}
-				pt("%d %s\n", what, msg)
-				conn.WriteMessage(what, msg)
-			}
-		*/
-		for {
-			ce(conn.WriteMessage(websocket.TextMessage, []byte("foo")), "write")
-			time.Sleep(time.Second)
-		}
-	})
-
-	http.HandleFunc("/jobs", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/jobs.json", func(w http.ResponseWriter, r *http.Request) {
+		var infos []TraceInfo
 		jobTraceSet.Dump(w, func(trace *Trace) bool {
 			return !trace.Flag("done")
+		}, func(t *Trace) {
+			infos = append(infos, TraceInfo{
+				What:    t.what,
+				Entries: t.Entries(),
+			})
 		})
+		bs, err := json.Marshal(infos)
+		ce(err, "marshal")
+		w.Write(bs)
 	})
-	http.HandleFunc("/done_jobs", func(w http.ResponseWriter, r *http.Request) {
+
+	http.HandleFunc("/done_jobs.json", func(w http.ResponseWriter, r *http.Request) {
+		var infos []TraceInfo
 		jobTraceSet.Dump(w, func(trace *Trace) bool {
 			return trace.Flag("done")
+		}, func(t *Trace) {
+			infos = append(infos, TraceInfo{
+				What:    t.what,
+				Entries: t.Entries(),
+			})
 		})
+		bs, err := json.Marshal(infos)
+		ce(err, "marshal")
+		w.Write(bs)
 	})
+
 }
